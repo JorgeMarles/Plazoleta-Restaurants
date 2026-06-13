a563abc408fa0358b4d815e638788ebcda048382
Jorge Marles <jamarlesf@gmail.com>
2026-06-12 23:18:32 -0500

    feat(infra/out/jpa): Jpa Implementation

---
+++ src/main/java/com/jamarlesf/plazoletarestaurants/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -0,0 +1,28 @@
+package com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.adapter;
+
+import com.jamarlesf.plazoletarestaurants.domain.model.Restaurant;
+import com.jamarlesf.plazoletarestaurants.domain.spi.IRestaurantPersistencePort;
+import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.entity.RestaurantEntity;
+import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
+import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.repository.IRestaurantRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
+
+    private final IRestaurantRepository restaurantRepository;
+    private final IRestaurantEntityMapper restaurantEntityMapper;
+
+    @Override
+    public void saveRestaurant(Restaurant restaurant) {
+        RestaurantEntity restaurantEntity = restaurantEntityMapper.toEntity(restaurant);
+        restaurantRepository.save(restaurantEntity);
+    }
+
+    @Override
+    public List<Restaurant> getRestaurants() {
+        return restaurantEntityMapper.toRestaurantList(restaurantRepository.findAll());
+    }
+}

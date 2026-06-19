1acaaba29a17e88d3437fd2a4fb176d6589e30b5
Jorge Marles <jamarlesf@gmail.com>
2026-06-18 20:48:46 -0500

    feat(app): implement Application layer

--- src/main/java/com/jamarlesf/plazoletarestaurants/application/handler/IDishHandler.java
+++ src/main/java/com/jamarlesf/plazoletarestaurants/application/handler/IDishHandler.java
@@ -1,6 +1,7 @@
 package com.jamarlesf.plazoletarestaurants.application.handler;
 
 import com.jamarlesf.plazoletarestaurants.application.dto.request.DishRequestDto;
+import com.jamarlesf.plazoletarestaurants.application.dto.request.UpdateDishRequestDto;
 import com.jamarlesf.plazoletarestaurants.application.dto.response.DishResponseDto;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface IDishHandler {
     void saveDish(DishRequestDto dishRequestDto);
     List<DishResponseDto> getDishes();
+    void updateDish(UpdateDishRequestDto updateDishRequestDto);
 }

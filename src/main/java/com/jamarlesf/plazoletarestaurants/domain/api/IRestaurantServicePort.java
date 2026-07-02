de6f7cc28b59b7627bf2c72392b9277227182a64
Jorge Marles <jamarlesf@gmail.com>
2026-07-02 18:21:21 -0500

    feat(domain): add api, spi and usecase methods

--- src/main/java/com/jamarlesf/plazoletarestaurants/domain/api/IRestaurantServicePort.java
+++ src/main/java/com/jamarlesf/plazoletarestaurants/domain/api/IRestaurantServicePort.java
@@ -1,10 +1,14 @@
 package com.jamarlesf.plazoletarestaurants.domain.api;
 
+import com.jamarlesf.plazoletarestaurants.domain.model.PageModel;
+import com.jamarlesf.plazoletarestaurants.domain.model.PaginationCriteria;
 import com.jamarlesf.plazoletarestaurants.domain.model.Restaurant;
+import com.jamarlesf.plazoletarestaurants.domain.model.RestaurantSortCriteria;
 
 import java.util.List;
 
 public interface IRestaurantServicePort {
     void save(Restaurant restaurant);
     List<Restaurant> findAll();
+    PageModel<Restaurant> getRestaurantsPaginatedAndSorted(PaginationCriteria pagination, RestaurantSortCriteria sortCriteria);
 }
--- src/main/java/com/jamarlesf/plazoletarestaurants/domain/spi/IRestaurantPersistencePort.java
+++ src/main/java/com/jamarlesf/plazoletarestaurants/domain/spi/IRestaurantPersistencePort.java
@@ -1,6 +1,9 @@
 package com.jamarlesf.plazoletarestaurants.domain.spi;
 
+import com.jamarlesf.plazoletarestaurants.domain.model.PageModel;
+import com.jamarlesf.plazoletarestaurants.domain.model.PaginationCriteria;
 import com.jamarlesf.plazoletarestaurants.domain.model.Restaurant;
+import com.jamarlesf.plazoletarestaurants.domain.model.RestaurantSortCriteria;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +12,5 @@ public interface IRestaurantPersistencePort {
     void save(Restaurant restaurant);
     List<Restaurant> findAll();
     Optional<Restaurant> findById(Long id);
+    PageModel<Restaurant> findAllPaginated(PaginationCriteria pagination, RestaurantSortCriteria sortCriteria);
 }

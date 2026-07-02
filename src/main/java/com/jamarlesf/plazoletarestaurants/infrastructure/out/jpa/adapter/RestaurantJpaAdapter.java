c1461f9b2462e4b0bf08291106099f93b7de55d3
Jorge Marles <jamarlesf@gmail.com>
2026-07-02 18:23:32 -0500

    feat(infra): implement endpoint and adapter

--- src/main/java/com/jamarlesf/plazoletarestaurants/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
+++ src/main/java/com/jamarlesf/plazoletarestaurants/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -1,11 +1,18 @@
 package com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.adapter;
 
+import com.jamarlesf.plazoletarestaurants.domain.model.PageModel;
+import com.jamarlesf.plazoletarestaurants.domain.model.PaginationCriteria;
 import com.jamarlesf.plazoletarestaurants.domain.model.Restaurant;
+import com.jamarlesf.plazoletarestaurants.domain.model.RestaurantSortCriteria;
 import com.jamarlesf.plazoletarestaurants.domain.spi.IRestaurantPersistencePort;
 import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.entity.RestaurantEntity;
 import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
 import com.jamarlesf.plazoletarestaurants.infrastructure.out.jpa.repository.IRestaurantRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +38,25 @@ public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
     public Optional<Restaurant> findById(Long id) {
         Optional<RestaurantEntity> restaurantEntity = restaurantRepository.findById(id);
         return restaurantEntity.map(restaurantEntityMapper::toRestaurant);
+    }
+
+    @Override
+    public PageModel<Restaurant> findAllPaginated(PaginationCriteria paginationCriteria, RestaurantSortCriteria sortCriteria) {
+        Pageable pageable = PageRequest.of(
+                paginationCriteria.getPageNumber(),
+                paginationCriteria.getPageSize(),
+                Sort.by(sortCriteria.sortField()).ascending()
+        );
+        Page<RestaurantEntity> page = restaurantRepository.findAll(pageable);
+        
+        return new PageModel<>(
+                restaurantEntityMapper.toRestaurantList(page.getContent()),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast()
+        );
     }
 }

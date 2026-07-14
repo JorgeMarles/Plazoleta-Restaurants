79c1b70aef2a3fc80ecd0d4c125d03b23fe4d429
Jorge Marles <jamarlesf@gmail.com>
2026-07-14 11:16:33 -0500

    feat(application): implement order delivered handler and pin request dto

--- src/main/java/com/jamarlesf/plazoletarestaurants/application/handler/IOrderHandler.java
+++ src/main/java/com/jamarlesf/plazoletarestaurants/application/handler/IOrderHandler.java
@@ -1,7 +1,6 @@
 package com.jamarlesf.plazoletarestaurants.application.handler;
 
 import com.jamarlesf.plazoletarestaurants.application.dto.request.OrderRequestDto;
-
 import com.jamarlesf.plazoletarestaurants.application.dto.response.OrderResponseDto;
 import com.jamarlesf.plazoletarestaurants.application.dto.response.PageResponseDto;
 
@@ -10,4 +9,5 @@ public interface IOrderHandler {
     PageResponseDto<OrderResponseDto> getOrdersByRestaurantAndStatus(Long restaurantId, String status, Integer page, Integer size);
     void assignOrder(Long orderId, Long employeeId);
     void markAsReady(Long orderId);
+    void markAsDelivered(Long orderId, String pin);
 }
